1faaa4c5cfadadd4158db9338fa71be80eca0021
Zurc <cruzjurado@gmail.com>
2017-11-29 13:14:58 +0000

    Add moment-example file

---
+++ moment-examples.js
@@ -0,0 +1,18 @@
+var moment = require('moment');
+var now = moment();
+
+// current time
+// console.log(now.format())
+// format using unix timestamp (timezone independent)
+// console.log(now.format('X'));
+// console.log(now.format('x'));
+// console.log(now.valueOf());
+
+var timestamp = 1511960931329;
+var timestampMoment = moment.utc(timestamp);
+
+console.log(timestampMoment.local().format('h:mm a'));
+
+// formatting time
+// ref: https://momentjs.com/docs/#/displaying/
+// console.log(now.format('MMM Do YYYY, h:mma'))
